7a9fb8ec512f089763c44e42347989c4132855cd
cds2005 <charliedsmith2005@gmail.com>
2024-04-09 17:00:29 -0600

    game.js - create upgrd2 function

--- game.js
+++ game.js
@@ -12,6 +12,8 @@ class Game{
     scoreAdd;
     health;
     standardHealth;
+    upgrdValues;
+
     constructor(){
         this.score = 0;
         this.dmg = 1;
@@ -19,6 +21,7 @@ class Game{
         this.health = 10;
         this.standardHealth = 10;
         this.buttons = new Map();
+        this.upgrdValues = [1,1,1,1];
         document.querySelectorAll('.butbut').forEach((el) => {
               this.buttons.set(el.id, new Button(el));
           });
@@ -40,18 +43,38 @@ class Game{
         document.getElementById(id).textContent = x;
     }
     upgrd1(){
-
+        if (this.score >= this.upgrdValues[0]**2){
+            this.score = this.score-(this.upgrdValues[0]**2);
+            this.updateText("officialScore", this.score);
+            this.upgrdValues[0]+=1;
+            this.updateText("upgrade1Cost",this.upgrdValues[0]**2)
+            this.updateText("upgrade1Current",(this.upgrdValues[0]-1)*10)
+        }
     }
     upgrd2(){
-
+        if (this.score >= this.upgrdValues[1]**3){
+            this.score = this.score-(this.upgrdValues[1]**3);
+            this.updateText("officialScore", this.score);
+            this.upgrdValues[1]+=1;
+            this.dmg +=this.upgrdValues[1]**2
+            this.updateText("upgrade2Cost",this.upgrdValues[1]**3)
+            this.updateText("upgrade2Current",this.dmg)
+        }
     }
     upgrd3(){
-
-    }
-    upgrd4(){
-
+        if (this.score >= this.upgrdValues[1]**3){
+            this.score = this.score-(this.upgrdValues[1]**3);
+            this.updateText("officialScore", this.score);
+            this.upgrdValues[1]+=1;
+            this.dmg +=this.upgrdValues[1]**2
+            this.updateText("upgrade2Cost",this.upgrdValues[1]**3)
+            this.updateText("upgrade2Current",this.dmg)
+        }
     }
     prestige(){
+
+    }
+    save(){
 
     }
     
