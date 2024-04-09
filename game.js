6b7a5dda42efc84371c7cfda3c94da836cc71654
cds2005 <charliedsmith2005@gmail.com>
2024-04-08 18:00:22 -0600

    game.js - made clicking the main button impact the score

--- game.js
+++ game.js
@@ -1,8 +1,29 @@
-class Buttons{
-
+class Button{
+    constructor(el){
+        this.el = el
+    }
 }
 
 
 class Game{
-
+    buttons;
+    score;
+    constructor(){
+        this.score = 0;
+        this.buttons = new Map();
+        document.querySelectorAll('.butbut').forEach((el) => {
+              this.buttons.set(el.id, new Button(el));
+          });
+    }
+    press(button){
+        this.score++;
+        this.updateScore(score)
+    }
+    updateScore() {
+        const scoreEl = document.getElementById('officialScore');
+        scoreEl.textContent = this.score;
+    }
+    
 }
+
+const game = new Game();
