2e27ef788e317dc1d7b8c04fe4e95a9d4d0034df
cds2005 <charliedsmith2005@gmail.com>
2024-04-08 18:15:24 -0600

    game.js - main button rework

--- game.js
+++ game.js
@@ -8,16 +8,34 @@ class Button{
 class Game{
     buttons;
     score;
+    dmg;
+    scoreAdd;
+    health;
+    standardHealth;
     constructor(){
         this.score = 0;
+        this.dmg = 1;
+        this.scoreAdd = 1;
+        this.health = 10;
+        this.standardHealth = 10;
         this.buttons = new Map();
         document.querySelectorAll('.butbut').forEach((el) => {
               this.buttons.set(el.id, new Button(el));
           });
     }
-    press(button){
-        this.score++;
-        this.updateScore(score)
+    pressMain(button){
+        this.updateHealth()
+    }
+    updateHealth(){
+        this.health = this.health-this.dmg;
+
+        if (this.health <1){
+            this.score += this.scoreAdd;
+            this.updateScore();
+            this.health = this.standardHealth;
+        }
+        const healthEl = document.getElementById("officialHealth");
+        healthEl.textContent = this.health
     }
     updateScore() {
         const scoreEl = document.getElementById('officialScore');
