a1dc054c56ab27d542c6d3f60f3d5032028588fb
cds2005 <charliedsmith2005@gmail.com>
2024-04-09 21:45:14 -0600

    home.js - made example for js login data collection

---
+++ home.js
@@ -0,0 +1,8 @@
+function login() {
+    const nameEl = document.querySelector("#username");
+    const passEl = document.querySelector("#password");
+    localStorage.setItem("username", nameEl.value);
+    localStorage.setItem("password",passEl.value)
+    window.location.href = "game.html";
+}
+  
