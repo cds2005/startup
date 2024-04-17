fce7832894b83c6e67c9167082d4e40139d9ed99
cds2005 <charliedsmith2005@gmail.com>
2024-04-17 12:02:18 -0600

    edit small errors with game

--- public/login.js
+++ public/login.js
@@ -1,7 +1,7 @@
 (async () => {
-    const userName = localStorage.getItem('username');
+    const username = localStorage.getItem('username');
     if (username) {
-      document.querySelector('#playerName').textContent = userName;
+      document.querySelector('#playerName').textContent = username;
       setDisplay('loginControls', 'none');
       setDisplay('playControls', 'block');
     } else {
@@ -19,19 +19,19 @@
   }
   
   async function loginOrCreate(endpoint) {
-    const userName = document.querySelector('#userName')?.value;
-    const password = document.querySelector('#userPassword')?.value;
+    const userName = document.querySelector('#username')?.value;
+    const password = document.querySelector('#password')?.value;
     const response = await fetch(endpoint, {
       method: 'post',
-      body: JSON.stringify({ email: userName, password: password }),
+      body: JSON.stringify({ name: username, password: password }),
       headers: {
         'Content-type': 'application/json; charset=UTF-8',
       },
     });
   
     if (response.ok) {
-      localStorage.setItem('userName', userName);
-      window.location.href = 'play.html';
+      localStorage.setItem('username', username);
+      window.location.href = 'game.html';
     } else {
       const body = await response.json();
       const modalEl = document.querySelector('#msgModal');
@@ -42,11 +42,11 @@
   }
   
   function play() {
-    window.location.href = 'play.html';
+    window.location.href = 'game.html';
   }
   
   function logout() {
-    localStorage.removeItem('userName');
+    localStorage.removeItem('username');
     fetch(`/api/auth/logout`, {
       method: 'delete',
     }).then(() => (window.location.href = '/'));
